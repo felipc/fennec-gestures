219999cda99569c628f4090fb794c55576320228
Felipe Gomes <felipc@gmail.com>
2009-02-25 00:30:00 -0300

    Assigning gestures as currently discussed on bugzilla
    Left and Right to back and forward, Up and Down to previous/next tab, Rotate Clockwise and Anticlockwise to zoom in and out.

--- chrome/content/ux.js
+++ chrome/content/ux.js
@@ -3,15 +3,46 @@
 function setGestureListeners() {
   window.addEventListener("Gesture_Star", function() { BrowserUI.doCommand("cmd_star"); }, false);
   window.addEventListener("Gesture_Wave", function() { BrowserUI.doCommand("cmd_star"); NotificationSystem.notify("Bookmarked"); }, false);
-  window.addEventListener("Gesture_RotateClockwise", function() { BrowserUI.newTab() }, false);
-  window.addEventListener("Gesture_RotateAnticlock", function() { document.getElementById("pieMenu").hidden=false; }, false);
-  window.addEventListener("Gesture_X", function() { BrowserUI.closeTab(Browser._currentTab); }, false);
   window.addEventListener("Gesture_U", function() { BrowserUI.showBookmarks(); }, false);
   window.addEventListener("Gesture_Eight", function() { NotificationSystem.notify("Hello World"); }, false);
   window.addEventListener("Gesture_Twirl", function() { NotificationSystem.notify("Do the twist!"); }, false);
   window.addEventListener("Gesture_House", function() { getBrowser().loadURI("http://www.mozilla.com", null, null, false); }, false);
   window.addEventListener("Gesture_Infinity", function() { NotificationSystem.notify("∞"); }, false);
   window.addEventListener("Gesture_Square", function() { NotificationSystem.notify("Square!"); }, false);
+
+  window.addEventListener("Gesture_X", function() { BrowserUI.closeTab(Browser._currentTab); }, false);
+  window.addEventListener("Gesture_RotateClockwise", function() { Browser.canvasBrowser.zoom(-1) }, false);
+  window.addEventListener("Gesture_RotateAnticlock", function() { Browser.canvasBrowser.zoom( 1) }, false);
+  window.addEventListener("Gesture_Left", function() { BrowserUI.doCommand("cmd_back"); }, false);
+  window.addEventListener("Gesture_Right", function() { BrowserUI.doCommand("cmd_forward"); }, false);
+
+  window.addEventListener("Gesture_Up", function() {
+    
+    let i, goal;
+    for (i = 0; i < Browser._tabs.length; i++) {
+      if (Browser._currentTab == Browser._tabs[i]) {
+        goal = i - 1;
+        break;
+      }
+    }
+    if (goal < 0) goal = Browser._tabs.length - 1;
+    
+    Browser.selectTab(Browser._tabs[goal]);
+    
+  }, false);  
+  window.addEventListener("Gesture_Down", function() {
+
+    let i, goal;
+    for (i = 0; i < Browser._tabs.length; i++) {
+      if (Browser._currentTab == Browser._tabs[i]) {
+        goal = i + 1;
+        break;
+      }
+    }
+    if (goal >= Browser._tabs.length) goal = 0;
+    
+    Browser.selectTab(Browser._tabs[goal]);
+  }, false);
 }
 
 function setPieMenu() {
